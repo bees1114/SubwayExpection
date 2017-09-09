d7337ea31806996c154257ca2ca4617c1986e917
bees1114 <bees1114@naver.com>
2017-09-09 16:45:25 +0900

    Add expectation data and update javascript

--- importPrediction.py
+++ importPrediction.py
@@ -1,15 +1,17 @@
 import csv
 import django
 import os
+from datetime import datetime
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SubwayExpectionWeb.settings")
 django.setup()
+from map.models import ExpectatedPassenger
 from map.models import Station
-FILE_DIR = 'D:/git/SubwayExpection/Data/'
+FILE_DIR = 'D:\git\SubwayExpection\Data'
 
 
 station_list = []
-with open(FILE_DIR + 'output_station.txt') as stations_file:
+with open(FILE_DIR + '\output_station.txt') as stations_file:
     input_station = stations_file.readlines()
     station = input_station[0]
     station = station.split()
@@ -20,23 +22,20 @@ with open(FILE_DIR + 'output_station.txt') as stations_file:
         st = st.strip(']')
         st = st[1:-1]
         station_list.append(st)
+    station_list.sort()
+    print(station_list)
 
+hours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 print(station_list.__len__())
-with open(FILE_DIR + 'station.csv', "r", encoding='utf-8') as file:
+with open('test_input_on_web.csv', "r",  encoding='utf-8') as file:
     reader = csv.reader(file)
-    for row in reader:
-        if row[1] not in station_list:
-            continue
-        else:
-            station_list.remove(row[1])
-        if row[0] == '\ufeff"전철역코드"':
-            continue
-
-
-        _, created = Station.objects.get_or_create(
-            code=row[4],
-            station_name=row[1],
-            location_x=row[7],
-            location_y=row[8]
-        )
+    for row, hour in zip(reader, hours):
+        for station, i in zip(station_list, range(station_list.__len__())):
+            row[i] = row[i].replace(u'\ufeff', '')
+            _, created = ExpectatedPassenger.objects.get_or_create(
+                station=Station.objects.get(station_name=station),
+                date=datetime(year=2017, month=9, day=1, hour=hour, minute=30),
+                on_passenger=-1,
+                off_passenger=row[i],
+            )
     print(station_list)
--- map/views.py
+++ map/views.py
@@ -4,5 +4,5 @@ from datetime import datetime
 
 def display(request):
     station = Station.objects.all()
-    expectatedPassenger = ExpectatedPassenger.objects.all()
+    expectatedPassenger = ExpectatedPassenger.objects.filter(datetime=='2017-09-01-08:30')
     return render(request, 'map/display.html', {'station':station, 'expectatedPassenger':expectatedPassenger})
