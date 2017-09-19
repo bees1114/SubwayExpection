030d543ce316dfd692e9509670a64de1c8cd6dcc
bees1114 <bees1114@naver.com>
2017-09-19 20:07:55 +0900

    검색기능 추가

--- map/urls.py
+++ map/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.display, name='display')
+    url(r'^$', views.display, name='display'),
 ]
--- map/views.py
+++ map/views.py
@@ -5,14 +5,26 @@ from django.utils import timezone
 import pytz
 
 def display(request):
-    station = Station.objects.all()
+    start_date = datetime(2017, 9, 1, 12, 30)
+    date_string = str(start_date.year) + "년 " + str(start_date.month) + "월 " + \
+                  str(start_date.day) + "일 " + str(start_date.hour) + "시 기준"
+
+    if request.method == 'GET':
+        try:
+            station_name = request.GET['station_name']
+            searched_station = Station.objects.get(station_name=station_name)
+            station_expectedPassenger = ExpectatedPassenger.objects.get(station=searched_station, date=start_date)
+        except:
+            station_expectedPassenger = None
+
     '''
     datetime_object = datetime(2017, 9, 1, 8, 30)
     pytz.timezone("Asia/Seoul").localize(datetime_object)
     '''
-    start_date = datetime(2017, 9, 1, 12, 30)
-    date_string = str(start_date.year) + "년 " + str(start_date.month) + "월 " + \
-                  str(start_date.day) + "일 " + str(start_date.hour) + "시 기준"
     expectedPassenger = ExpectatedPassenger.objects.filter(date=start_date)
 
-    return render(request, 'map/display.html', {'time': start_date, 'expectedPassenger': expectedPassenger})
+    return render(request, 'map/display.html', {
+        'time': start_date,
+        'expectedPassenger': expectedPassenger,
+        'searched_expectedPassenger': station_expectedPassenger,
+    })
